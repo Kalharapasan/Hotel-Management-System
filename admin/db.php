5e337124ffb3e4433cfe5f42989613d6fcda73d1
Pasan Kalhara <kalharap84@gmail.com>
2026-06-09 12:30:52 +0530

    Add all file Complete

---
+++ admin/db.php
@@ -0,0 +1,16 @@
+<?php
+
+$user = "root";
+$pass = "";
+$host = "localhost";
+$database = "hms_db";
+
+$conn = new mysqli($host, $user, $pass, $database);
+
+if ($conn->connect_error) {
+    die("Could not connect to MySQL: " . $conn->connect_error);
+}
+
+echo "Connected Successfully";
+
+?>
