52244d33938c176add9e5004fd32dceb09d6f09c
Pasan Kalhara <kalharap84@gmail.com>
2026-06-09 12:52:46 +0530

    Add index.php, footer.php, and head.php files with initial structure

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+    	header.masthead {
+		  background: url(assets/img/<?php echo $_SESSION['setting_cover_img'] ?>);
+		  background-repeat: no-repeat;
+		  background-size: cover;
+		}
+    </style>
+</head>
+<body>
     
-?>
+</body>
+</html>
