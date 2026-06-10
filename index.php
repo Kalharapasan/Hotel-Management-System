914ca5af36fb196eb29c77b4e963acee5bddfbc4
Pasan Kalhara <kalharap84@gmail.com>
2026-06-10 11:34:03 +0530

    Refactor project structure: remove unused files and implement Router class
    - Deleted CSS file (styles.css) and PHP files (header.php, footer.php, home.php, index.php) to clean up the project.
    - Removed multiple image files from the images directory to reduce clutter.
    - Deleted SQL file (HMS.sql) containing database schema and sample data.
    - Introduced a new Router class in Core/Router.php to handle routing logic.

--- index.php
+++ index.php
@@ -1,28 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-    <?php
-    session_start();
-    include('header.php');
-    include('config/db.php');
+<?php
+session_start();
 
-	$query = $conn->query("SELECT * FROM system_settings limit 1")->fetch_array();
-	foreach ($query as $key => $value) {
-		if(!is_numeric($key))
-			$_SESSION['setting_'.$key] = $value;
-	}
-    ?>
+require_once 'config/db.php';
+use App\Core\Router;
 
-    <style>
-    	header.masthead {
-		  background: url(assets/img/<?php echo $_SESSION['setting_cover_img'] ?>);
-		  background-repeat: no-repeat;
-		  background-size: cover;
-		}
-    </style>
-    <body id="page-top">
-       
-    </body>
+$router = new Router();
 
-    <?php $conn->close() ?>
-
-</html>
+?>
