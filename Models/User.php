3e14a2fec9598fbad7b4ac6bebd594f1d60de8ff
Pasan Kalhara <kalharap84@gmail.com>
2026-06-14 01:14:56 +0530

    Complete usermodel

--- Models/User.php
+++ Models/User.php
@@ -2,28 +2,14 @@
 
 namespace App\Models;
 
-class User extends BaseModel
-{
-    public function getById($id)
-    {
-        $id = (int) $id;
-
-        return $this->db
-            ->query("SELECT * FROM users WHERE id=$id")
-            ->fetch_assoc();
+class User extends BaseModel {
+    public function getById($id) {
+        return $this->db->query("SELECT * FROM users WHERE id = $id")->fetch_assoc();
     }
 
-    public function update($id, $data)
-    {
-        $id = (int) $id;
-
-        $fullname = $data['fullname'];
-        $email = $data['email'];
-
-        return $this->db->query("
-            UPDATE users 
-            SET fullname='$fullname', email='$email' 
-            WHERE id=$id
-        ");
+    public function update($id, $data) {
+        $fullname = $this->db->real_escape_string($data['fullname']);
+        $email = $this->db->real_escape_string($data['email']);
+        return $this->db->query("UPDATE users SET fullname = '$fullname', email = '$email' WHERE id = $id");
     }
 }
